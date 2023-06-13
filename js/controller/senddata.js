cf507d6861dd06085501118242ed643dce201842
christyuda <yudacihuy8@gmail.com>
2023-06-13 11:36:53 +0700

    send data success

--- js/controller/senddata.js
+++ js/controller/senddata.js
@@ -1,63 +1,64 @@
+document.getElementById("submitButton").addEventListener("click", function(event) {
+  event.preventDefault();
+
+  const mhswid = document.getElementById("inputMhswID").value;
+  const dosenid = document.getElementById("inputDosenID").value;
+  const tahunid = document.getElementById("inputTahunID").value;
+  const tipe = document.getElementById("inputTipe").value;
+  const pertemuan = parseInt(document.getElementById("inputPertemuan").value);
+  const nilai = parseInt(document.getElementById("inputNilai").value);
+  const topik = document.getElementById("inputTopik").value;
+  const tanggal = document.getElementById("inputTanggal").value;
+  const penilai = document.getElementById("inputPenilai").value;
+  const log = document.getElementById("inputLog").value;
+  
 
 
-document.getElementById("bimbinganForm").addEventListener("submit", function(event) {
-    event.preventDefault();
-  
-    const mhswid = document.getElementById("inputMhswID").value;
-    const dosenid = document.getElementById("inputDosenID").value;
-    const tahunid = document.getElementById("inputTahunID").value;
-    const tipe = document.getElementById("inputTipe").value;
-    const pertemuan = document.getElementById("inputPertemuan").value;
-    const nilai = document.getElementById("inputNilai").value;
-    const topik = document.getElementById("inputTopik").value;
-    const tanggal = document.getElementById("inputTanggal").value;
-    const penilai = document.getElementById("inputPenilai").value;
+  if (mhswid === '' || dosenid === '' || tahunid === '' || tipe === '' || pertemuan === '' || nilai === '' || topik === '' || tanggal === '' || penilai === '' || log === '') {
+      console.log('Mohon lengkapi semua field');
+      return;
+  }
 
-    if (mhswid === '' || dosenid === '' || tahunid === '' || tipe === '' || pertemuan === '' || nilai === '' || topik === '' || tanggal === '' || penilai === '') {
-        console.log('Mohon lengkapi semua field');
-        return;
-      }
-    addBimbingan(mhswid, dosenid, tahunid, tipe, pertemuan, nilai, topik, tanggal, penilai); 
-  
-    document.getElementById("bimbinganForm").reset();
-  });
-  
-  function addBimbingan(mhswid, dosenid, tahunid, tipe, pertemuan, nilai, topik, tanggal, penilai) {
-    const url = 'https://hris_backend.ulbi.ac.id/bim/databimbingan/';
-    const data = {
+  addBimbingan(mhswid, dosenid, tahunid, tipe, pertemuan, nilai, topik, tanggal, penilai, log); 
+
+  document.getElementById("bimbinganForm").reset();
+});
+
+function addBimbingan(mhswid, dosenid, tahunid, tipe, pertemuan, nilai, topik, tanggal, penilai, log) {
+  const url = 'https://hris_backend.ulbi.ac.id/bim/databimbingan/';
+
+  // Generate a random 5-digit number
+
+  const data = {
       MhswID: mhswid,
       DosenID: dosenid,
       TahunID: tahunid,
       Tipe: tipe,
-      Pertemuan: pertemuan,
+      Pertemuan_: pertemuan,
       Nilai: nilai, 
       Topik: topik,
       Tanggal: tanggal,
-      Penilai: penilai
+      Penilai: penilai,
+      Log:log
+  };
 
-    };
-  
-    fetch(url, {
+  fetch(url, {
       method: 'POST',
       headers: {
-        'Content-Type': 'application/json',
+          'Content-Type': 'application/json',
       },
       body: JSON.stringify(data)
-    })
-    .then(response => {
+  })
+  .then(response => {
       if (response.ok) {
-        console.log('Data berhasil dipost');
-        document.getElementById('successMessage').textContent = 'Data berhasil dipost';
-
+          console.log('Data berhasil dipost');
+          document.getElementById('successMessage').textContent = 'Data berhasil dipost';
       } else {
-
-        console.log('Terjadi kesalahan saat memposting data');
+          console.log('Terjadi kesalahan saat memposting data');
       }
-    })
-    .catch(error => {
+  })
+  .catch(error => {
       console.log('Terjadi kesalahan:', error);
       document.getElementById('errorMessage').textContent = 'Terjadi kesalahan: ' + error.message;
-
-    });
-  }
-  
+  });
+}
